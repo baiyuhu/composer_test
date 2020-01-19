98cd39c537ebc5d4097cd2f6d97803cd36193101
caihu <1059150180@qq.com>
2020-01-19 15:21:26 +0800

    'init'

---
+++ src/Flower/Lily.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caihu
+ * Date: 2020/1/19
+ * Time: 15:14
+ */
+
+namespace Flower;
+
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caihu
+ * Date: 2020/1/19
+ * Time: 15:13
+ */
+
+namespace Flower;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
